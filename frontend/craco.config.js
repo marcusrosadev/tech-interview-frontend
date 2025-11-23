5d5b7254a87c27909802d283380051b37d309a4d
marcusrosadev <marcusrosadev@gmail.com>
2025-11-23 04:12:14 -0300

    feat: finish unity tests, polish documentation and last enviroment configurations to go live

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -12,5 +12,19 @@ module.exports = {
       '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@appTypes$': '<rootDir>/src/types/index',
+        '^@appTypes/(.*)$': '<rootDir>/src/types/$1',
+        '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+        '^@mocks/(.*)$': '<rootDir>/src/mocks/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+      },
+    },
+  },
 };
 
